7e2d00fd33684084a41e3018c60803aef82762d8
DSG (Destitute-Streetdwelling-Guttersnipe) <90743025+Destitute-Streetdwelling-Guttersnipe@users.noreply.github.com>
2021-09-15 12:24:16 +0700

    code refactor: rename variables

--- reghex_patcher.py
+++ reghex_patcher.py
@@ -10,130 +10,130 @@ def PatchFile(input_file):
        file.write(data)
     print(f"[+] Patched file saved to {input_file}")
 
-def FindRegHex(sig, data):
-    matches = list(re.finditer(sig.reghex, data, re.DOTALL | re.VERBOSE))[:10] # only 10 matches
+def FindRegHex(fix, data):
+    matches = list(re.finditer(fix.reghex, data, re.DOTALL | re.VERBOSE))[:10] # only 10 matches
     if len(matches) == 0: return None
-    print("[-] Found at {}: pattern {}".format(','.join([hex(m.start()) for m in matches]), sig.name))
+    print("[-] Found at {}: pattern {}".format(','.join([hex(m.start()) for m in matches]), fix.name))
     return matches[0] 
 
 def Patch(data):
-    for sig in Sigs().Load(data):
-        match = FindRegHex(sig, data)
-        if not match: exit("[!] Can not find pattern: {} '{}'".format(sig.name, sig.reghex))
+    for fix in Fixes().Load(data):
+        match = FindRegHex(fix, data)
+        if not match: exit("[!] Can not find pattern: {} '{}'".format(fix.name, fix.reghex))
         offset = match.start()
-        if sig.is_ref: offset = RelativeOffset(offset, data)
-        print("[+] Patch at {}: {}\n".format(hex(offset), sig.fix.hex(' ')))
-        data[offset : offset + len(sig.fix)] = sig.fix
+        if fix.is_ref: offset = RelativeOffset(offset, data)
+        print("[+] Patch at {}: {}\n".format(hex(offset), fix.patch.hex(' ')))
+        data[offset : offset + len(fix.patch)] = fix.patch
 
 def RelativeOffset(offset, data):
     relative_address = int.from_bytes(data[offset : offset + 4], byteorder='little') # address size is 4 bytes
     return (offset + 4 + relative_address) & 0xFFFFFFFF
 
-class Sigs:
+class Fixes:
     nop5 = "90" * 5 # nop over E8 . . . . (call [dword])
     ret = "C3" # ret
     ret0 = "48 31 C0 C3" # xor rax, rax; ret
     ret1 = "48 31 C0 48 FF C0 C3" # xor rax, rax; inc rax; ret
     ret281 = "48 C7 C0 19 01 00 00 C3" # mov rax, 281; ret
 
-    class Sig:
+    class Fix:
         nop5 = "90" * 5 # nop over E8 . . . . (call [dword])
         def __init__(self, name, reghex, patch=nop5, is_ref=False): # reghex is regex with hex bytes
             self.name = name
             self.reghex = bytes(re.sub(r"\b([0-9a-fA-F]{2})\b", r"\\x\1", reghex), encoding='utf-8') # escape hex bytes
             self.is_ref = is_ref
-            self.fix = bytes.fromhex(fix)
-    # NOTE: license_notify was said to use fix=ret0 !
-    st_linux_sigs = [
-        Sig(name="license_check", reghex="(?<= E8 ) . . . . . . . . . . . . .", fix=ret0, is_ref=True),
-        Sig(name="server_validate", reghex="55 . . . . . . . . . . .", fix=ret1),
-        Sig(name="license_notify", reghex="41 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .", fix=ret),
-        Sig(name="crash_reporter", reghex="55 . . . . . . . . . . . . . . . . . . . . . .", fix=ret),
-        Sig(name="invalidate1", reghex="E8 . . . . . . . . . . . ."),
-        Sig(name="invalidate2", reghex="E8 . . . . . . . . . . . . . . . ."),
+            self.patch = bytes.fromhex(patch)
+    # NOTE: license_notify was said to use patch=ret0 !
+    st_linux_fixes = [
+        Fix(name="license_check", reghex="(?<= E8 ) . . . . . . . . . . . . .", patch=ret0, is_ref=True),
+        Fix(name="server_validate", reghex="55 . . . . . . . . . . .", patch=ret1),
+        Fix(name="license_notify", reghex="41 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .", patch=ret),
+        Fix(name="crash_reporter", reghex="55 . . . . . . . . . . . . . . . . . . . . . .", patch=ret),
+        Fix(name="invalidate1", reghex="E8 . . . . . . . . . . . ."),
+        Fix(name="invalidate2", reghex="E8 . . . . . . . . . . . . . . . ."),
     ]
-    st_macos_sigs = [
-        Sig(name="license_check", reghex="(?<= E8 ) . . . . . . . . . . . . .", fix=ret0, is_ref=True),
-        Sig(name="server_validate", reghex="55 . . . . . . . . . . . . . . . . .", fix=ret1),
-        Sig(name="license_notify", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .", fix=ret),
-        Sig(name="crash_reporter", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . . .", fix=ret),
-        Sig(name="invalidate1", reghex="E8 . . . . . . . . . . . . . ."),
-        Sig(name="invalidate2", reghex="E8 . . . . . . . . . . . . . . . . . ."),
+    st_macos_fixes = [
+        Fix(name="license_check", reghex="(?<= E8 ) . . . . . . . . . . . . .", patch=ret0, is_ref=True),
+        Fix(name="server_validate", reghex="55 . . . . . . . . . . . . . . . . .", patch=ret1),
+        Fix(name="license_notify", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .", patch=ret),
+        Fix(name="crash_reporter", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . . .", patch=ret),
+        Fix(name="invalidate1", reghex="E8 . . . . . . . . . . . . . ."),
+        Fix(name="invalidate2", reghex="E8 . . . . . . . . . . . . . . . . . ."),
     ]
-    st_wind_sigs = [
-        Sig(name="license_check", reghex="(?<= E8 ) . . . . . . . . . . . . .", fix=ret0, is_ref=True),
-        Sig(name="server_validate", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . . . .", fix=ret1),
-        Sig(name="license_notify", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .", fix=ret),
-        Sig(name="crash_reporter", reghex="41 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .", fix=ret),
-        Sig(name="invalidate1", reghex="(?<= . . . . . . ) E8 . . . . (48|49) . ."), # 48 for dev, 49 for stable
-        Sig(name="invalidate2", reghex="(?<= . . . . . . ) E8 . . . . . . . . . (48|4C) . . ."), # 48 for dev, 4C for stable
+    st_wind_fixes = [
+        Fix(name="license_check", reghex="(?<= E8 ) . . . . . . . . . . . . .", patch=ret0, is_ref=True),
+        Fix(name="server_validate", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . . . .", patch=ret1),
+        Fix(name="license_notify", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .", patch=ret),
+        Fix(name="crash_reporter", reghex="41 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .", patch=ret),
+        Fix(name="invalidate1", reghex="(?<= . . . . . . ) E8 . . . . (48|49) . ."), # 48 for dev, 49 for stable
+        Fix(name="invalidate2", reghex="(?<= . . . . . . ) E8 . . . . . . . . . (48|4C) . . ."), # 48 for dev, 4C for stable
     ]
-    sm_linux_sigs = [
-        Sig(name="server_validate", reghex="55 . . . . . . . . . . .", fix=ret1),
-        Sig(name="license_notify", reghex="41 . . . . . . . . . . . . . . . . .", fix=ret),
-        Sig(name="crash_reporter", reghex="55 . . . . . . . . . . . . . . . . . . . . . .", fix=ret),
-        Sig(name="invalidate1", reghex="E8 . . . . . . . . . . . ."),
-        Sig(name="invalidate2", reghex="E8 . . . . . . . . . . . . . . . ."),
+    sm_linux_fixes = [
+        Fix(name="server_validate", reghex="55 . . . . . . . . . . .", patch=ret1),
+        Fix(name="license_notify", reghex="41 . . . . . . . . . . . . . . . . .", patch=ret),
+        Fix(name="crash_reporter", reghex="55 . . . . . . . . . . . . . . . . . . . . . .", patch=ret),
+        Fix(name="invalidate1", reghex="E8 . . . . . . . . . . . ."),
+        Fix(name="invalidate2", reghex="E8 . . . . . . . . . . . . . . . ."),
     ]
-    sm_linux_sigs_stable = [
-        Sig(name="license_check", reghex="(?<= E8 ) . . . . . . . . . . .", fix=ret281, is_ref=True),
+    sm_linux_fixes_stable = [
+        Fix(name="license_check", reghex="(?<= E8 ) . . . . . . . . . . .", patch=ret281, is_ref=True),
     ]
-    sm_linux_sigs_dev = [
-        Sig(name="license_check", reghex="(?<= E8 ) . . . . . . . . .", fix=ret1, is_ref=True),
+    sm_linux_fixes_dev = [
+        Fix(name="license_check", reghex="(?<= E8 ) . . . . . . . . .", patch=ret1, is_ref=True),
     ]
-    sm_macos_sigs = [
-        Sig(name="server_validate", reghex="55 . . . . . . . . . . . . . . . . .", fix=ret1),
-        Sig(name="license_notify", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .", fix=ret),
-        Sig(name="crash_reporter", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . . .", fix=ret),
-        Sig(name="invalidate1", reghex="E8 . . . . . . . . . . . . . ."),
-        Sig(name="invalidate2", reghex="E8 . . . . . . . . . . . . . . . . . ."),
+    sm_macos_fixes = [
+        Fix(name="server_validate", reghex="55 . . . . . . . . . . . . . . . . .", patch=ret1),
+        Fix(name="license_notify", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .", patch=ret),
+        Fix(name="crash_reporter", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . . .", patch=ret),
+        Fix(name="invalidate1", reghex="E8 . . . . . . . . . . . . . ."),
+        Fix(name="invalidate2", reghex="E8 . . . . . . . . . . . . . . . . . ."),
     ]
-    sm_macos_sigs_stable = [
-        Sig(name="license_check", reghex="(?<= E8 ) . . . . . . . . . . .", fix=ret281, is_ref=True),
+    sm_macos_fixes_stable = [
+        Fix(name="license_check", reghex="(?<= E8 ) . . . . . . . . . . .", patch=ret281, is_ref=True),
     ]
-    sm_macos_sigs_dev = [
-        Sig(name="license_check", reghex="(?<= E8 ) . . . . . . . . .", fix=ret1, is_ref=True),
+    sm_macos_fixes_dev = [
+        Fix(name="license_check", reghex="(?<= E8 ) . . . . . . . . .", patch=ret1, is_ref=True),
     ]
-    sm_wind_sigs = [
-        Sig(name="server_validate", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . . . .", fix=ret1),
-        Sig(name="license_notify", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . .", fix=ret),
-        Sig(name="crash_reporter", reghex="41 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .", fix=ret),
-        Sig(name="invalidate1", reghex="(?<= . . . . . . ) E8 . . . . . . . . . ."),
-        Sig(name="invalidate2", reghex="(?<= . . . . . . ) E8 . . . . . . . . . . ."),
+    sm_wind_fixes = [
+        Fix(name="server_validate", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . . . .", patch=ret1),
+        Fix(name="license_notify", reghex="55 . . . . . . . . . . . . . . . . . . . . . . . .", patch=ret),
+        Fix(name="crash_reporter", reghex="41 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .", patch=ret),
+        Fix(name="invalidate1", reghex="(?<= . . . . . . ) E8 . . . . . . . . . ."),
+        Fix(name="invalidate2", reghex="(?<= . . . . . . ) E8 . . . . . . . . . . ."),
     ]
-    sm_wind_sigs_stable = [
-        Sig(name="license_check", reghex="(?<= E8 ) . . . . . . . . . . .", fix=ret281, is_ref=True),
+    sm_wind_fixes_stable = [
+        Fix(name="license_check", reghex="(?<= E8 ) . . . . . . . . . . .", patch=ret281, is_ref=True),
     ]
-    sm_wind_sigs_dev = [
-        Sig(name="license_check", reghex="(?<= E8 ) . . . . . . . . . . . . . .", fix=ret1, is_ref=True),
+    sm_wind_fixes_dev = [
+        Fix(name="license_check", reghex="(?<= E8 ) . . . . . . . . . . . . . .", patch=ret1, is_ref=True),
     ]
-    tagged_sigs = [
-        (dict(arch=b"x64", app="SublimeText", channel=b"dev", os=b"windows"), st_wind_sigs),
-        (dict(arch=b"x64", app="SublimeText", channel=b"dev", os=b"osx"), st_macos_sigs),
-        (dict(arch=b"x64", app="SublimeText", channel=b"dev", os=b"linux"), st_linux_sigs),
-        (dict(arch=b"x64", app="SublimeText", channel=b"stable", os=b"windows"), st_wind_sigs),
-        (dict(arch=b"x64", app="SublimeText", channel=b"stable", os=b"osx"), st_macos_sigs),
-        (dict(arch=b"x64", app="SublimeText", channel=b"stable", os=b"linux"), st_linux_sigs),
-        (dict(arch=b"x64", app="SublimeMerge", channel=b"dev", os=b"windows"), sm_wind_sigs_dev + sm_wind_sigs),
-        (dict(arch=b"x64", app="SublimeMerge", channel=b"dev", os=b"osx"), sm_macos_sigs_dev + sm_macos_sigs),
-        (dict(arch=b"x64", app="SublimeMerge", channel=b"dev", os=b"linux"), sm_linux_sigs_dev + sm_linux_sigs),
-        (dict(arch=b"x64", app="SublimeMerge", channel=b"stable", os=b"windows"), sm_wind_sigs_stable + sm_wind_sigs),
-        (dict(arch=b"x64", app="SublimeMerge", channel=b"stable", os=b"osx"), sm_macos_sigs_stable + sm_macos_sigs),
-        (dict(arch=b"x64", app="SublimeMerge", channel=b"stable", os=b"linux"), sm_linux_sigs_stable + sm_linux_sigs),
+    tagged_fixes = [
+        (dict(arch=b"x64", app="SublimeText", channel=b"dev", os=b"windows"), st_wind_fixes),
+        (dict(arch=b"x64", app="SublimeText", channel=b"dev", os=b"osx"), st_macos_fixes),
+        (dict(arch=b"x64", app="SublimeText", channel=b"dev", os=b"linux"), st_linux_fixes),
+        (dict(arch=b"x64", app="SublimeText", channel=b"stable", os=b"windows"), st_wind_fixes),
+        (dict(arch=b"x64", app="SublimeText", channel=b"stable", os=b"osx"), st_macos_fixes),
+        (dict(arch=b"x64", app="SublimeText", channel=b"stable", os=b"linux"), st_linux_fixes),
+        (dict(arch=b"x64", app="SublimeMerge", channel=b"dev", os=b"windows"), sm_wind_fixes_dev + sm_wind_fixes),
+        (dict(arch=b"x64", app="SublimeMerge", channel=b"dev", os=b"osx"), sm_macos_fixes_dev + sm_macos_fixes),
+        (dict(arch=b"x64", app="SublimeMerge", channel=b"dev", os=b"linux"), sm_linux_fixes_dev + sm_linux_fixes),
+        (dict(arch=b"x64", app="SublimeMerge", channel=b"stable", os=b"windows"), sm_wind_fixes_stable + sm_wind_fixes),
+        (dict(arch=b"x64", app="SublimeMerge", channel=b"stable", os=b"osx"), sm_macos_fixes_stable + sm_macos_fixes),
+        (dict(arch=b"x64", app="SublimeMerge", channel=b"stable", os=b"linux"), sm_linux_fixes_stable + sm_linux_fixes),
     ]
     detects = [
-        Sig(name="SublimeText", reghex=r"/updates/4/(?P<channel>\w+)_update_check\?version=\d+&platform=(?P<os>\w+)&arch=(?P<arch>\w+)"),
-        Sig(name="SublimeMerge", reghex=r"/updates/(?P<channel>\w+)_update_check\?version=\d+&platform=(?P<os>\w+)&arch=(?P<arch>\w+)"),
+        Fix(name="SublimeText", reghex=r"/updates/4/(?P<channel>\w+)_update_check\?version=\d+&platform=(?P<os>\w+)&arch=(?P<arch>\w+)"),
+        Fix(name="SublimeMerge", reghex=r"/updates/(?P<channel>\w+)_update_check\?version=\d+&platform=(?P<os>\w+)&arch=(?P<arch>\w+)"),
     ]
 
     def Load(self, data):
-        for sig in self.detects:
-            m = FindRegHex(sig, data)
-            if m: detected = { "app": sig.name, **m.groupdict() }
+        for fix in self.detects:
+            m = FindRegHex(fix, data)
+            if m: detected = { "app": fix.name, **m.groupdict() }
         print(f"[+] Detected tags: {detected}\n")
-        for tags, sigs in self.tagged_sigs:
-            if tags == detected: return sigs
-        exit("[!] Can not find sigs for target file")
+        for tags, fixes in self.tagged_fixes:
+            if tags == detected: return fixes
+        exit("[!] Can not find fixes for target file")
 
 def main():
     print(credits)
