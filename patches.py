674ead92e84dd34b2cab4c88828bc51d50aff8d1
DSG (Destitute-Streetdwelling-Guttersnipe) <90743025+Destitute-Streetdwelling-Guttersnipe@users.noreply.github.com>
2022-01-13 16:19:53 +0700

    add reghex patterns for popup About

--- patches.py
+++ patches.py
@@ -42,7 +42,7 @@ string_detections = [ # detect string in data & code sections
 ]
 ref_detections = [
     # detection for number, string and function inside AMD64 instructions
-    Fix(name="amd64", reghex=r"  (?<= C7 84 . .{4} | .{3} C7 44 . . ) . |" ## mov dword ptr [r? + r? + ?], ?
+    Fix(name="amd64", reghex=r"  (?<= C7 84 . .{4} | .{3} C7 44 . . | . C7 85 .{4} ) . |" ## mov dword ptr [r? + r? + ?], ?
                           + r"(?<= . . 8D 87 | . 41 8D 8F | 41 8D 8C 24 | . . 81 [FC FF] ) . |" ## lea eax, [rdi+?]; lea ecx, [r1?+?]; lea ecx, [r12+?]; cmp edi, ?; cmp r?d, ?;
                           + r"(?<= . 41 BE | 41 81 C6 | . 81 [C5 C7 FB FD FF] | 8D 9C 09 ) . |" ## mov e?, ?; mov r14d, ?; add r14d, ?; add e?, ?; cmp e?, ?; lea ebx, [rcx+rcx+?]
                           + r"(?<=  8A [80-84 86-8C 8E-94 96-97] | . [B8-BB BD-BF 3D] ) . |" ## mov ?l, byte ptr [r? + ?]; mov e?, ?; mov r?, ?; cmp eax, ?
