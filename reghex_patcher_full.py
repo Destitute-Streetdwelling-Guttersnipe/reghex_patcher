a6dbfa67ccc04d782b5b6504412f760df602d8e9
DSG (Destitute-Streetdwelling-Guttersnipe) <90743025+Destitute-Streetdwelling-Guttersnipe@users.noreply.github.com>
2024-06-03 07:50:05 +0700

    check if input data is empty

--- reghex_patcher_full.py
+++ reghex_patcher_full.py
@@ -109,7 +109,7 @@ def FindFixes(file):
     return [fix for fix in sum(fixes, []) if fix.arch in [None, file.arch] and fix.os in [None, file.os]] # filter out different arch & os
 
 def UnpackAndPatch(data):
-    (magic, num_archs) = struct.unpack(">2L", data[:(start := 4*2)])
+    (magic, num_archs) = struct.unpack(">2L", data[:(start := 4*2)]) if len(data) >= 4*2 else (0, 0)
     if magic == 0xCAFEBABE: # FAT_MAGIC of MacOS universal binary
         while (num_archs := num_archs - 1) >= 0:
             (cpu_type, _, offset, size, _) = struct.unpack(">5L", data[start : (start := start + 4*5)])
