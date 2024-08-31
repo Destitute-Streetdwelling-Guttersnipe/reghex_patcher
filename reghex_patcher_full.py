ef3c43bed43cea0dc912c1113498a20af2e945fe
DSG (Destitute-Streetdwelling-Guttersnipe) <90743025+Destitute-Streetdwelling-Guttersnipe@users.noreply.github.com>
2024-08-31 11:59:19 +0700

    non-existent element in array fix.patch is considered to be an empty string

--- reghex_patcher_full.py
+++ reghex_patcher_full.py
@@ -28,11 +28,11 @@ def ApplyFix(fix, patched, file, refs, match = None, fn = None):
                 p = Position(file, address=Ref2Address(p0.address, p0.offset, file))
             if p0.address and not fix.look_behind:
                 ref0 = refs[p0.address] = fix.name if i == 0 else '.'.join(fix.name.split('.')[0:i+1:i]) # extract part 0 and part i from fix.name if i > 0
-                if not refs.get(p.address) and fix.patch[i-1] == '\r': break # skip a match if referenced address is not found earlier and its patch is '\r'
+                if not refs.get(p.address) and fix.patch[i-1:i] == ['\r']: break # skip a match if referenced address is not found earlier and its patch is '\r'
                 if not refs.get(p.address): refs[p.address] = '.'+fix.name.split('.')[i] # extract part i from fix.name
-                if (fn := 1) and fix.patch: PatchAtOffset(p.offset, file, patched, fix.patch, i, p.ref_info(p0, ref0), match)
+                if (fn := 1) and fix.patch != '': PatchAtOffset(p.offset, file, patched, fix.patch, i, p.ref_info(p0, ref0), match)
             else: fn = FindNearestFunction(file, refs, p0, p, fn)
-    if fix.patch and (not match or not fn): print(f"[!] Cannot find pattern: {fix.name} {fix.reghex}")
+    if fix.patch != '' and (not match or not fn): print(f"[!] Cannot find pattern: {fix.name} {fix.reghex}")
 
 def FindNearestFunction(file, refs, p0, p, fn):
     if (ref0 := refs.get(p0.address)) or (ref := refs.get(p.address)): # look behind if p0 or p is in refs
@@ -41,7 +41,7 @@ def FindNearestFunction(file, refs, p0, p, fn):
     return fn
 
 def PatchAtOffset(offset, file, patched, patch, i, ref_info, match):
-    h = patch[i-1] if isinstance(patch, list) else patch
+    h = ''.join(patch[i-1:i]) if isinstance(patch, list) else patch # non-existent element in array fix.patch is considered to be an empty string
     print(f"[+] Patch at {ref_info} = {h}" if h else f"[-] Found at {ref_info} : {match.group(i).hex(' ')}") # show matched bytes if h is empty
     if (b := bytes.fromhex(h)) and offset: patched[(o := offset + file.base_offset) : o + len(b)] = b # has no effect if h is empty or h contains spaces
 
